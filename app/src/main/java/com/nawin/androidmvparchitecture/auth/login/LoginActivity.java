294bd6f9c58ce6233b6598c3095dfed1d9252749
nawinkhatiwada <nabin.khatiwada@braindigit.com>
2017-06-13 15:37:55 +0545

    Login activity layout design done

--- app/src/main/java/com/nawin/androidmvparchitecture/auth/login/LoginActivity.java
+++ app/src/main/java/com/nawin/androidmvparchitecture/auth/login/LoginActivity.java
@@ -11,7 +11,7 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Vi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         presenter = new LoginPresenter(this);
     }
 

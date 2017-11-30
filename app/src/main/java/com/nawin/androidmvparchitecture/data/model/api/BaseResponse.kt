b54511ed0335fd3c383808db7e63d12eafe158c4
nawinkhatiwada <nabin.khatiwada@braindigit.com>
2017-11-30 14:31:08 +0545

    base response redundant code removed

--- app/src/main/java/com/nawin/androidmvparchitecture/data/model/api/BaseResponse.kt
+++ app/src/main/java/com/nawin/androidmvparchitecture/data/model/api/BaseResponse.kt
@@ -3,9 +3,6 @@ package com.nawin.androidmvparchitecture.data.model.api
 /**
  * Created on 11/30/17.
  */
-class BaseResponse<T> {
-
-    var statusMessage: String? = null
-    var statusCode: Int = 0
-    var response: T? = null
-}
+data class BaseResponse<T>(var statusMessage: String? = null,
+                           var statusCode: Int = 0,
+                           var response: T? = null)
